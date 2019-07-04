935a88a4994a565e2ee4aad923bf9864bb563b27
Seth NeJame <sethnejame@gmail.com>
2019-07-04 19:28:22 +0200

    All tests passing (create new article, article confirmation, etc)

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -21,17 +21,17 @@ Given("I visit the {string} page") do |string|
 end
 
 When("I click {string} link") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  click_on string
 end
 
-When("I fill in {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I fill in {string} with {string}") do |field, input|
+  fill_in field, with: input
 end
 
-When("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click {string} button") do |button|
+  click_on button
 end
 
-Then("I should be on {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should be on {string} page") do |content|
+  expect(page).to have_content content
 end

46792f2a4f8754a16ef55b06ecfd1029f9a554ba
Seth NeJame <sethnejame@gmail.com>
2019-07-06 21:02:08 +0200

    Adding comment functionality, testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'articles/index'
   root controller: :articles, action: :index
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
 end
